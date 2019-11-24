20342dda0e19df5546113186ff3c86a3f98f6e87
Umesh <uchhatre@gmail.com>
2019-11-23 23:28:10 -0500

    added isupport for legacy proxy auth

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ fi
 cp /opt/newrelic/newrelic.yml /opt/newrelic/newrelic.yml.orig
 cat /opt/newrelic/newrelic.yml.orig  | sed "s=My Application=$APP_NAME="  | sed "s|<%= license_key %>|$NR_KEY|" > /opt/newrelic/newrelic.yml
 
-java -Xms512m -Xmx1024m -javaagent:/opt/newrelic/newrelic.jar -jar mysbapplication.jar
+java -Xms512m -Xmx1024m -Djdk.http.auth.tunneling.disabledSchemes="" -javaagent:/opt/newrelic/newrelic.jar -jar mysbapplication.jar
