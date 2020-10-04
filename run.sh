9d3d29e1fc393146f697354393d736e9b4d1b748
Umesh <uchhatre@gmail.com>
2020-10-03 23:56:29 -0400

    updated to java 14

--- run.sh
+++ run.sh
@@ -9,7 +9,10 @@ if [ -n "$PARENT_HOST" ]; then
   echo "$PARENT_HOST parent-host" >> /etc/hosts
 fi
 
-cp /opt/newrelic/newrelic.yml /opt/newrelic/newrelic.yml.orig
-cat /opt/newrelic/newrelic.yml.orig  | sed "s=My Application=$APP_NAME="  | sed "s|<%= license_key %>|$NR_KEY|" > /opt/newrelic/newrelic.yml
+mkdir -p /opt/newrelic
+cp /tmp/newrelic-agent-6.1.0.jar /opt/newrelic/newrelic.jar
+
+cp /tmp/newrelic.yml /tmp/newrelic.yml.orig
+cat /tmp/newrelic.yml.orig  | sed "s=My Application=$APP_NAME="  | sed "s|<%= license_key %>|$NR_KEY|" > /opt/newrelic/newrelic.yml
 
 java -Xms512m -Xmx1024m -Djdk.http.auth.tunneling.disabledSchemes="" -javaagent:/opt/newrelic/newrelic.jar -jar mysbapplication.jar
